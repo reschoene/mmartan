2725f67e5196ed38da8f0db4d95c3a04b45362ad
Renato Schoene <reschoene@gmail.com>
2020-04-05 19:47:05 -0300

    remoção de import desnecessário

--- frontend/src/components/PaginationBarComponent/PaginationBar.js
+++ frontend/src/components/PaginationBarComponent/PaginationBar.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import Pagination from "react-js-pagination";
 import './PaginationBar.scss';
-import "bootstrap/dist/css/bootstrap.min.css";
 
 class PaginationBar extends Component{
     constructor(props) {
